ea74bd8ea19cca602c2ea234a50e24d3c8e55e8a
PMOz-tech <ozon.oghenemaro@gmail.com>
2021-11-03 16:57:45 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace BankTeller
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            Teller teller = new Teller();
+            teller.CustomerInfo();
+        }
+    }
+}
---
+++ Teller.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankTeller
+{
+    class Teller 
+    {
+        public string Name { get; set; }
+
+      
+
+        public double CustomerInfo()
+        {
+            Console.WriteLine("What is your name?");
+            Name = Console.ReadLine();
+            Console.WriteLine("How much do you want to deposit?");
+            var depositAmount = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("What type of account do you want to open?/n 1.Savings /n 2. Current /n 3. Kids Account");
+            var  accountType = Console.ReadLine();
+            if (accountType == "Savings")
+            {
+                Teller teller = new Teller();
+                teller.SavingInterest(depositAmount);
+            }
+            return depositAmount;
+        }
+
+
+        public void SavingInterest(double depositAmount)
+        {
+
+          var  intrest = depositAmount / 100;
+            Console.WriteLine("Your rate is " + intrest);
+
+         
+
+        }
+
+        //public double  vatRate(double rate)
+        //{
+        //    double rateVat = rate;
+        //    rateVat = 7.5;
+
+        //    return rateVat;
+
+            
+            
+
+        }
+
+        
+    }
+
+   
+}
